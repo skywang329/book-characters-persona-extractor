91b7cfee3b63c623fefdd6cdcbe4f046d0cb7039
NikhilPr95 <nikhill.prabhu@gmail.com>
2016-07-07 15:55:12 +0530

    Added attribute p (paragraphId) to Quotation

--- src/novels/Quotation.java
+++ src/novels/Quotation.java
@@ -5,7 +5,8 @@ public class Quotation {
 	public int end;
 	public int attributionId;
 	public int sentenceId;
-
+	public int p;
+	
 	public Quotation(int start, int end, int sentenceId) {
 		this.start = start;
 		this.end = end;
